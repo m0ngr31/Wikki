c6f81fb92ef000aab54ea6d1bd080ef5aa165675
Joe Ipson <joeipson@gmail.com>
2015-08-11 00:29:03 -0600

    Initial commit

---
+++ wikia Unofficial/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using wikia_Unofficial.Pages;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace wikia_Unofficial
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.Phone.UI.Input.HardwareButtons"))
+            {
+                title.Margin = new Thickness(0, 0, 0, 400);
+                main_button.Margin = new Thickness(0, 400, 0, 0);
+            } else
+            {
+                title.Margin = new Thickness(0, 0, 0, 500);
+                main_button.Margin = new Thickness(0, 500, 0, 0);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //main_button.Foreground = new SolidColorBrush(Colors.White);
+            this.Frame.Navigate(typeof(home));
+        }
+    }
+}
---
+++ wikia Unofficial/Templates/SplitPanel.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using wikia_Unofficial.Pages;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace wikia_Unofficial.Templates
+{
+    public sealed partial class SplitPanel : UserControl
+    {
+        public SplitView MySplitView { get; set; }
+        //public event EventHandler ButtonClicked;
+        Frame rootFrame = Window.Current.Content as Frame;
+        public SplitPanel()
+        {
+            this.InitializeComponent();
+            (this.Content as FrameworkElement).DataContext = this;
+        }
+
+        //private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        //}
+
+        private void goto_About(object sender, RoutedEventArgs e)
+        {
+            //this.Frame.Navigate(typeof(about));
+            rootFrame.Navigate(typeof(about));
+        }
+
+        private void goto_Search(object sender, RoutedEventArgs e)
+        {
+            //this.Frame.Navigate(typeof(search));
+            rootFrame.Navigate(typeof(search));
+        }
+
+        private void goto_Home(object sender, RoutedEventArgs e)
+        {
+            //this.Frame.Navigate(typeof(article));
+            rootFrame.Navigate(typeof(home));
+        }
+    }
+}
