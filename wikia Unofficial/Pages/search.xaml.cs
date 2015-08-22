135ef3e93e2d0710a3dfe024689faef396ba34ea
Joe Ipson <joeipson@gmail.com>
2015-08-22 01:56:26 -0600

    Changed color scheme up a little bit. Added Search glyph to search textbox. Few UI changes.

--- wikia Unofficial/Pages/search.xaml.cs
+++ wikia Unofficial/Pages/search.xaml.cs
@@ -23,6 +23,7 @@ namespace wikia_Unofficial.Pages
     /// </summary>
     public sealed partial class search : Page
     {
+        public double barHeight;
         public search()
         {
             this.InitializeComponent();
@@ -31,6 +32,36 @@ namespace wikia_Unofficial.Pages
         private void HamburgerButton_Click(object sender, RoutedEventArgs e)
         {
             MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // SystemControlBackgroundChromeWhiteBrush //
+            searchGlyph.Foreground = (Application.Current.Resources["SystemControlHighlightAccentBrush"] as Brush);
+            searchGrid.Background = (Application.Current.Resources["SystemControlBackgroundChromeWhiteBrush"] as Brush);
+            //Close commandbar when there is a focus
+            if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.Phone.UI.Input.HardwareButtons"))
+                commandBar.Height = 0;
+        }
+
+        private void TextBox_Loaded(object sender, RoutedEventArgs e)
+        {
+            searchBox.Focus(FocusState.Programmatic);
+        }
+
+        private void searchBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.Phone.UI.Input.HardwareButtons"))
+                commandBar.Height = barHeight;
+
+            //TextBoxButtonBackgroundThemeBrush
+            searchGrid.Background = (Application.Current.Resources["TextBoxButtonBackgroundThemeBrush"] as Brush);
+            searchGlyph.Foreground = (Application.Current.Resources["SystemControlPageTextBaseMediumBrush"] as Brush);
+        }
+
+        private void commandBar_Loaded(object sender, RoutedEventArgs e)
+        {
+            barHeight = commandBar.Height;
         }
     }
 }
